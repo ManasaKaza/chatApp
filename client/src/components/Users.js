303fef12ab70d3316ab9ada2712156d87ef01484
Manasa Kaza <122222373+ManasaKaza@users.noreply.github.com>
2026-05-17 17:36:55 +0530

    Update Users.js

--- client/src/components/Users.js
+++ client/src/components/Users.js
@@ -34,7 +34,7 @@ function Users() {
         Authorization: `Bearer ${userData.data.token}`,
       },
     };
-    axios.get("http://localhost:8080/user/fetchUsers", config).then((data) => {
+    axios.get("`${API_URL}/user/fetchUsers", config).then((data) => {
       console.log("UData refreshed in Users panel ");
       setUsers(data.data);
       // setRefresh(!refresh);
