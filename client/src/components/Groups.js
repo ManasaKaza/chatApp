d19d1bf00c27c329d8d8e3b9c2075cc25bf3d117
Manasa Kaza <122222373+ManasaKaza@users.noreply.github.com>
2026-05-17 17:37:22 +0530

    Update Groups.js

--- client/src/components/Groups.js
+++ client/src/components/Groups.js
@@ -36,7 +36,7 @@ function Groups() {
     };
 
     axios
-      .get("http://localhost:8080/chat/fetchGroups", config)
+      .get("`${API_URL}/chat/fetchGroups", config)
       .then((response) => {
         console.log("Group Data from API ", response.data);
         SetGroups(response.data);
